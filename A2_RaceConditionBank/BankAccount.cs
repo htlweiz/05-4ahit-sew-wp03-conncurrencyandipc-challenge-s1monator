46b7864e4cba5d8906cfc5200e2431f5f5b5ce4e
s1monator <simonopriessnig07@gmail.com>
2025-11-10 08:54:36 +0100

    Aufgabe 4

--- A2_RaceConditionBank/BankAccount.cs
+++ A2_RaceConditionBank/BankAccount.cs
@@ -5,25 +5,40 @@ namespace A2_RaceConditionBank;
 public class BankAccount
 {
     private int balance;
+    private static object lockObject = new object();
    
     
     public BankAccount(int initial) 
     { 
+        lock (lockObject)
+        {
         balance = initial; 
+        }
     }
     
     public void Deposit(int amount) 
     { 
+        
+        lock (lockObject)
+        {
+            balance += amount;
+        }
+        
+        
        
     }
     
     public void Withdraw(int amount) 
-    { 
-        
+    {
+        lock (lockObject){
+        balance -= amount; 
+        }
     }
     
     public int GetBalance() 
     {
+        lock (lockObject){
         return balance;
+        }
     }
 }

49040632dbd66845c271a74304ae4e8f41b26a75
s1monator <simonopriessnig07@gmail.com>
2025-11-10 08:04:05 +0100

    Aufgabe 1

--- A1_ZweiThreadsZaehlenWinner/Program.cs
+++ A1_ZweiThreadsZaehlenWinner/Program.cs
@@ -10,17 +10,33 @@ class Program
     public static void Main(string[] args)
     {
         Console.WriteLine("Übung 1: Zwei Threads – Zählen & Winner");
+        thread1 = new Thread(CountUpThreadA);
+        thread2 = new Thread(CountDownThreadB);
+        thread1.Start();
+        thread2.Start();
+
+        thread1.Join();
+        thread2.Join();
+        
         
         
     }
     
     private static void CountUpThreadA()
     {
-        
+        for (int i = 1; i <= 100; i++)
+        {
+            Console.WriteLine($"{i}");
+            Thread.Sleep(100);
+        }    
     }
     
     private static void CountDownThreadB()
     {
-       
+        for (int i = 100; i >= 1; i--)
+        {
+            Console.WriteLine($"{i}");
+            Thread.Sleep(100);
+        }
     }
 }
